812d5d6bb2f2479351800ed2eb78c26019f3ab91
Oscar Diaz <oscar.dh@outlook.com>
2022-02-13 19:06:16 -0600

    added default port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const expressApp = require("./index.js");
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 const server = http.createServer(expressApp);
 
 console.log(`Server liseting in port: ${port}`)
