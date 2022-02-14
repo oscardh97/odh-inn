dc0a1550efaa23a306628f46f00864823b26c160
Oscar Diaz <oscar.dh@outlook.com>
2022-02-13 18:53:39 -0600

    Added Documentation

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# odh-inn
-Hotel simulation in Node
+# Hotel Simulation
+
+
+Thechnolgies used:
+
+- Node
+## Features
+
+- Load data
+- Hotel simulation
+
+
+## Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+npm i
+npm run dev
+```
+
+For production environments...
+
+```sh
+npm i
+npm start
+```
+## Use
+There's only one endpoint that runs the simulation.
+#### Method: Get
+#### URL:
+```sh
+localhost:8080/api/simulation/run
+```
+
+## Questions
+1. How long did it take you to complete this assignment? Did you get stuck anywhere?
+    Around 6-8hrs. I didn't get stuck.
+2. - Please analyze the runtime complexity of your solution in terms of Big O.
+        The algorithm is O(n^2) since we want to sort the rooms by the lower cost so we have to sort the array, we could use a Merge algorithm to change the complexity to O(nLog(n). 
+    ```
+            suitableRooms.sort((a, b) => {
+                const cost1 = a.cleaning_fee + (a.daily_rate * stayDays);
+                const cost2 = b.cleaning_fee + (b.daily_rate * stayDays);
+                return cost1 - cost2;
+            });
+    ```
+    -  Is your algorithm fast enough for 1000 requests?
+        Yes, it is. The most complex thing is sorting suitableRooms and since is a subset of the rooms it wouldn't be a problem.
+3. Imagine a system in which it adjusted prices depending on various factors such as number of remaining availability, weekends, or whether there was an event in town that weekend. How might you accommodate this change through the schema?
+    - I'd modify the room schema to allow to stored different types of rates and also create a new Schema for Events. So when a  new request gets in, I'll check if there are events on those days and if there are we'll use the rate for that event type.
+    ### Room Schema
+```
+    {
+        id,
+        num_beds,
+        allow_smoking,
+        cleaning_fee,
+        rates: {
+            normal,
+            weekend,
+            holiday,
+            low_availability,
+            ...
+        }
+    }
+```
+
+
+### Event Schema
+```
+    {
+        start_date,
+        end_date,
+        rate_type (normal, weekend, holiday, low_availability, ...)
+    }
+```
--- src/controllers/simulation.js
+++ src/controllers/simulation.js
@@ -12,10 +12,20 @@ const requests = require("../../inputs/requests.json");
 const reservations = require("../../inputs/reservations.json");
 const rooms = require("../../inputs/rooms.json");
 
-const validateAnswers = (simulation) => {
-    return lodash.isEqual(simulation.answers, simulation.reservations);
+/**
+ * Check if the simulation reservations are correct
+ * @param {Object} simulation
+ * 
+ * @return {Boolean}
+ */
+const validateAnswers = ({ answers, reservations }) => {
+    return lodash.isEqual(answers, reservations);
 };
 
+/**
+ * Add Reservations to the simulation
+ * @param {Object[]} newReservations - Raw data Reservations
+ */
 const loadReservations = (newReservations = reservations) => {
     const validReservations = [];
     
@@ -29,6 +39,11 @@ const loadReservations = (newReservations = reservations) => {
 
     return validReservations;
 };
+
+/**
+ * Add rooms to the simulation and create a room map based on number of beds by room and if it allows smoking
+ * @param {Object[]} newRooms - Raw data rooms
+ */
 const loadRooms = (newRooms = rooms) => {
     const roomsMap = {};
     const validRooms = [];
@@ -53,6 +68,10 @@ const loadRooms = (newRooms = rooms) => {
     };
 };
 
+/**
+ * Add requests to the simulation
+ * @param {Object[]} newRequests - Raw data requests
+ */
 const loadRequests = (newRequests = requests) => {
     const _newRequests = [];
 
@@ -64,77 +83,119 @@ const loadRequests = (newRequests = requests) => {
     return newRequests;
 }
 
-const processRequests = (simulation) => {
-    const findRoomOptions = ({
-        min_beds,
-        is_smoker,
-        checkin_date,
-        checkout_date
-    }) => {
-        const subCategory = is_smoker ? "smoking" : "no_smoking";
-        const checkInDate = moment(checkin_date);
-        const checkOutDate = moment(checkout_date);
-        const stayDays = checkOutDate.diff(checkInDate, 'd');
-        let suitableRooms = simulation.roomsMap[2][subCategory];
-
-        if (min_beds === 1) {
-            suitableRooms = suitableRooms.concat(simulation.roomsMap[1][subCategory]);
-        }
-
-        suitableRooms.sort((a, b) => {
-            const cost1 = a.cleaning_fee + (a.daily_rate * stayDays);
-            const cost2 = b.cleaning_fee + (b.daily_rate * stayDays);
-
-            return cost1 - cost2;
-        });
-
-        return suitableRooms;
+/**
+ * Find the best room options for a requests
+ * @param {Request} 
+ * @param {Integer} Request.min_beds - Minimum number of beds that the guest would like to stay in (always 1 or 2). Guests requesting 1 bed may stay in either singles or doubles in rooms schema.
+ * @param {Boolean} Request.is_smoker - Whether the guest is a smoker. Must match to allow_smoking under rooms.
+ * @param {String} Request.checkin_date - Date of check in. Duration of the stay cannot overlap another reservation.
+ * @param {String} Request.checkout_date - Date of check out. Duration of the stay cannot overlap another reservation.
+ * 
+ * @return {Rooms[]} All suitables rooms by given request
+ */
+const findRoomOptions = (simulation, {
+    min_beds,
+    is_smoker,
+    checkin_date,
+    checkout_date
+}) => {
+    const subCategory = is_smoker ? "smoking" : "no_smoking";
+    const checkInDate = moment(checkin_date);
+    const checkOutDate = moment(checkout_date);
+    const stayDays = checkOutDate.diff(checkInDate, 'd');
+    let suitableRooms = simulation.roomsMap[2][subCategory];
+    
+    // When a guest requests a single, it may assign a double bed
+    if (min_beds === 1) {
+        suitableRooms = suitableRooms.concat(simulation.roomsMap[1][subCategory]);
     }
 
-    simulation.requests.forEach((_request) => {
-        const suitableRooms = findRoomOptions(_request);
+    // Sort the rooms array so whenever there are multiple available rooms for a request, assign the room with the lower final price. 
+    suitableRooms.sort((a, b) => {
+        const cost1 = a.cleaning_fee + (a.daily_rate * stayDays);
+        const cost2 = b.cleaning_fee + (b.daily_rate * stayDays);
 
-        for (let ROOM_INDEX in suitableRooms) {
-            const currentRoom = suitableRooms[ROOM_INDEX];
-            let available = false;
-            const checkInDate = moment(_request.checkin_date);
-            const checkOutDate = moment(_request.checkout_date);
-            let currentReservation = null;
+        return cost1 - cost2;
+    });
 
-            for (let RESERVATION_INDEX in simulation.reservations) {
-                if (simulation.reservations[RESERVATION_INDEX].room_id === currentRoom.id) {
-                    currentReservation = simulation.reservations[RESERVATION_INDEX];
-                }
-            }
+    return suitableRooms;
+};
 
-            if (currentReservation) {
-                const reservationCheckInDate = moment(currentReservation.checkin_date);
-                const reservationCheckOutDate = moment(currentReservation.checkout_date);
-                if (checkOutDate.diff(reservationCheckInDate) <= 0 || checkInDate.diff(reservationCheckOutDate) >= 0) {
-                    available = true;
-                }
+/**
+ * Find the best room option and add the new reservation
+ * @param {Object} simulation
+ * @param {Request} request
+ * @param {Rooms[]} suitableRooms - Rooms that matches guest request
+ */
+const findAvailableRoom = (simulation, request, suitableRooms) => {
+    for (let ROOM_INDEX in suitableRooms) {
+        const currentRoom = suitableRooms[ROOM_INDEX];
+        let available = false;
+        const checkInDate = moment(request.checkin_date);
+        const checkOutDate = moment(request.checkout_date);
+        let currentReservation = null;
 
-            }
-
-            if (!currentReservation || available) {
-                const stayDays = checkOutDate.diff(checkInDate, 'd');
-                const newReservation = reservationController.createReservation({
-                    room_id: currentRoom.id,
-                    checkin_date: _request.checkin_date,
-                    checkout_date: _request.checkout_date,
-                    total_charge: currentRoom.cleaning_fee + (currentRoom.daily_rate * stayDays)
-                });
-
-                if (newReservation !== null) {
-                    simulation.reservations.push(newReservation);
-                }
-
-                break;
+        // Check if there's a reservation for this room
+        for (let RESERVATION_INDEX in simulation.reservations) {
+            if (simulation.reservations[RESERVATION_INDEX].room_id === currentRoom.id) {
+                currentReservation = simulation.reservations[RESERVATION_INDEX];
             }
         }
+
+        // If there's a reservation for this room, checks if it's available by request dates
+        if (currentReservation) {
+            const reservationCheckInDate = moment(currentReservation.checkin_date);
+            const reservationCheckOutDate = moment(currentReservation.checkout_date);
+            if (checkOutDate.diff(reservationCheckInDate) <= 0 || checkInDate.diff(reservationCheckOutDate) >= 0) {
+                available = true;
+            }
+
+        }
+
+        // Create a new Reservation is the room is available
+        if (!currentReservation || available) {
+            const stayDays = checkOutDate.diff(checkInDate, 'd');
+            const newReservation = reservationController.createReservation({
+                room_id: currentRoom.id,
+                checkin_date: request.checkin_date,
+                checkout_date: request.checkout_date,
+                total_charge: currentRoom.cleaning_fee + (currentRoom.daily_rate * stayDays)
+            });
+
+            if (newReservation !== null) {
+                simulation.reservations.push(newReservation);
+            }
+
+            return newReservation;
+        }
+    }
+};
+
+/**
+ * Given a simulation, process all the requests
+ * @simulation {Object} Simulation params
+ */
+const processRequests = (simulation) => {
+
+    // Process requests one by one as if they were real time requests.
+    simulation.requests.forEach((_request) => {
+        const suitableRooms = findRoomOptions(simulation, _request); // Rooms that fits the guest request
+        findAvailableRoom(simulation, _request, suitableRooms);
+        
     });
 };
 
+/**
+ * Run the simulation with the given files in the inputs folder
+ * 
+ * @return {
+ *  @isValid {boolean} It's true in case answers and reservations arrays matches
+ *  @requests {Request[]} All the requests in this simulation
+ *  @rooms {Room[]} All the rooms in the hotel
+ *  @reservations {Reservation[]} All the reservations
+ *  @answers {Object[]} All the given answers
+ * }
+ */
 const run = () => {
     const newSimulation = {
         isValid: false,
@@ -147,7 +208,8 @@ const run = () => {
         },
         answers
     };
-
+    
+    // Load the input values
     const { roomsMap, validRooms } = loadRooms();
     newSimulation.roomsMap = roomsMap;
     newSimulation.rooms = validRooms;
@@ -155,7 +217,11 @@ const run = () => {
     newSimulation.reservations = loadReservations();
     
     newSimulation.requests = loadRequests();
+
+    //Process the given requests
     processRequests(newSimulation);
+
+    //Validate the processed requests
     newSimulation.isValid = validateAnswers(newSimulation);
     return newSimulation;
 };
